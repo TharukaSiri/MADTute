9845537a01abf6deb3a603c644d2c0d4493ba35d
TharukaSiri <it19039350@my.sliit.lk>
2020-08-08 20:19:44 +0530

    initial commit

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView2;
+
+    private static final String TAG = "Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView2 = findViewById(R.id.textView2);
-
         textView2.setText(R.string.Msg2);
+
+        Log.i(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
     }
 }
